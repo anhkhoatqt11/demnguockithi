2c67942179d170007bc5ac8d2da0571e7922854f
Trương Anh Khoa <anhkhoatqt11@gmail.com>
2021-12-18 21:05:49 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('/demnguockithi/sw.js')
         .then((reg) => console.log('Service Worker Register', reg))
         .catch((err) => console.log('Service Worker Not Register', err))
 }
