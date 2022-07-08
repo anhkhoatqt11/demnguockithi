1914f8cdc97b0966e528bac8de6f1966f697d8d2
Trương Anh Khoa <anhkhoatqt11@gmail.com>
2022-07-08 17:22:57 +0700

    Updated to National Exam 2023

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/demnguockithi/sw.js')
-        .then((reg) => console.log('Service Worker Register', reg))
-        .catch((err) => console.log('Service Worker Not Register', err))
-}
+
+caches.has('site-static-v18').then(function(hasCache) {
+    if (!hasCache) {
+    } else {
+        caches.delete("site-static-v18");
+        alert("Bạn đang ở phiên bản cũ của Website. Reload để cập nhật thời gian mới nhất");
+    }
+  }).catch(function() {
+    // Handle exception here.
+  });
+
+// if ('serviceWorker' in navigator) {
+//     navigator.serviceWorker.register('/sw.js')
+//         .then((reg) => console.log('Service Worker Register', reg))
+//         .catch((err) => console.log('Service Worker Not Register', err))
+// }
